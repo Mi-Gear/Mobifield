25923dac87e9efc366f67d38908f62abf1742329
Mi-Gear <dima.krasikov.03@inbox.ru>
2026-09-25 10:17:59 +1100

    Update reg_router.py

--- routers/reg_router.py
+++ routers/reg_router.py
@@ -21,7 +21,8 @@ async def on_enter(message:Pepe, state: FSMContext):
     reg_dialog = dialog["registration"]
     await state.set_state(Reg.goto_hub)
     photo = FSInputFile("images/guildmaster.jpg")
-    kb = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="Воин",callback_data="class_warrior")],[InlineKeyboardButton(text = "Стрелок",callback_data="class_archer")],[InlineKeyboardButton(text = "Маг",callback_data="class_mage")]])
+    #kb = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="Воин",callback_data="class_warrior")],[InlineKeyboardButton(text = "Стрелок",callback_data="class_archer")],[InlineKeyboardButton(text = "Маг",callback_data="class_mage")]])
+    kb = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="Я хочу вступить в гильдию!",callback_data="123")]])
     msg = await message.send_photo(caption=reg_dialog[0].format(name=message._get_user().first_name),photo=photo,reply_markup = kb)
     add_to_trash(message.get_user_id(),msg.message_id)
 

e414550aa185ab749d4f863e278a7f1b209f1d61
Mi-Gear <dima.krasikov.03@inbox.ru>
2026-09-25 10:58:49 +1100

    Update reg_router.py

--- routers/reg_router.py
+++ routers/reg_router.py
@@ -28,6 +28,7 @@ async def on_enter(message:Pepe, state: FSMContext):
 @reg.callback_query(StateFilter("reg_0"))
 @pepe_handler
 async def reg_1(message:Pepe, state: FSMContext):
+    print(message.callback_message.text)
     bot.delete_message(chat_id=message.callback_from.id, message_id=message.callback_message.message_id)
     await state.set_state("reg_1")
     photo = FSInputFile("images/guildmaster.jpg")
